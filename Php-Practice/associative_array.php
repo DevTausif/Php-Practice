b89459b4e025e0043439ad6b2663f2670db0f7d8
DevTausif <kabirtausif610@gmail.com>
2024-06-01 19:45:42 +0600

    print added

--- Php-Practice/associative_array.php
+++ Php-Practice/associative_array.php
@@ -4,10 +4,15 @@
         "Stan"=> 40,
         "Lee"=> 35,
     ];
+    //checking data type & array 
     echo"<pre>";
     echo "<h1>";
        print_r( $age );
        var_dump( $age);
     echo  "</h1>";
     echo"</pre>";
+    //print array's value
+    echo "<h1>".$age["Bill"]."</h1></br>";
+    echo "<h1>".$age["Stan"]."</h1></br>";
+    echo "<h1>".$age["Lee"]."</h1></br>";
 ?>
