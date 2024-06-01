01f5bf6d4a1beb645fa877affdde1d344b285a5a
DevTausif <kabirtausif610@gmail.com>
2024-06-01 19:42:13 +0600

    associative array

---
+++ Php-Practice/associative_array.php
@@ -0,0 +1,13 @@
+<?php
+    $age = [
+        "Bill"=> 25,
+        "Stan"=> 40,
+        "Lee"=> 35,
+    ];
+    echo"<pre>";
+    echo "<h1>";
+       print_r( $age );
+       var_dump( $age);
+    echo  "</h1>";
+    echo"</pre>";
+?>
