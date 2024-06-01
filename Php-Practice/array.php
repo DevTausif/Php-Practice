24e84dc71d1aca66640b5bd71d6d7ef1be36c7de
DevTausif <kabirtausif610@gmail.com>
2024-06-01 19:01:36 +0600

    array

--- Php-Practice/array.php
+++ Php-Practice/array.php
@@ -1,3 +1,12 @@
 <?php
-    
+    $colors = ["red", "blue","black","white"];
+
+    echo "<ul>";
+     for ($i = 0; $i < 4; $i++) 
+     {
+        echo "<h1>";
+        echo "<li>$colors[$i] </li>";
+        echo "</h1>";
+     }
+    echo "</ul>";
 ?>
